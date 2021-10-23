e0788cb428f888dc17d345b2142a7bf13885f09e
CarlosMbp <cm.barrameda.2018@alumnos.urjc.es>
2021-10-23 10:32:17 +0200

    Warnings
    Some warnings have been fixed
    
    Algunos warnings corregigos

--- app/src/main/java/com/example/quizapp/QuizActivity.java
+++ app/src/main/java/com/example/quizapp/QuizActivity.java
@@ -1,9 +1,6 @@
 package com.example.quizapp;
 
-import static android.R.*;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.res.ColorStateList;
@@ -13,7 +10,6 @@ import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -75,7 +71,7 @@ public class QuizActivity extends AppCompatActivity {
     //Pasamos a la siguiente pregunta
     public void siguiente(View view){
         txtResultado.setVisibility(View.INVISIBLE);
-        if (contestada==false){
+        if (!contestada){
             if (rdb1.isChecked() || rdb2.isChecked() || rdb3.isChecked() || rdb4.isChecked()){
                 comprobarRespuesta();
                 contTiempo.cancel();
